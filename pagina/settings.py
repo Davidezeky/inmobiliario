6cb8341daf642c37980aaf1917b0324259668972
david_romero <davidezequielromerovasquez@gmail.com>
2024-06-13 04:50:14 -0400

    hito 2

--- pagina/settings.py
+++ pagina/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'pagina.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -133,7 +133,7 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = 'media'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'web/templates/static'),
+    os.path.join(BASE_DIR, 'web/templates/vendor'),
 )
 
 # Default primary key field type
--- query.py
+++ query.py
@@ -6,23 +6,25 @@ from django.db import connection
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Configurar el entorno de Django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pagina.settings')
 django.setup()
 
 # Cambiar al directorio del proyecto Djangom 
 os.chdir(BASE_DIR)
 
-
+ 
 
 def fetch_data_from_db():
     with connection.cursor() as cursor:
         # Ejecutar la consulta SQL
         query = """
-        SELECT 
-          *
+         SELECT 
+            r.nombre AS nombre_region
         FROM 
-        web_inmueble
-           
+            web_region r
+        ORDER BY 
+            r.nombre ASC
+        
         """
         cursor.execute(query)
         # Obtener todos los resultados
@@ -40,7 +42,7 @@ def export_data_to_txt():
     rows = fetch_data_from_db()
 
     # Paso 2: Guardar los datos en un archivo de texto
-    file_path = 'solicitudes_arriendo.txt'
+    file_path = os.path.join(BASE_DIR, 'PORTAL_INMOBILIARIO', 'listado_de_inmuebles_regiones.txt')
     save_data_to_txt(rows, file_path)
 
     print(f"Data saved to {file_path}")
