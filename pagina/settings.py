a7345006cc7fdc2e662f10891a66ae66884d48c8
David Romero <davidezequielromerovasquez@gmail.com>
2024-10-16 13:43:03 -0300

    Actualiza configuración y estilos

--- pagina/settings.py
+++ pagina/settings.py
@@ -132,13 +132,13 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = "/media/"
-MEDIA_ROOT = 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, ''),
+    os.path.join(BASE_DIR, 'web/templates/static'),
 )
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
